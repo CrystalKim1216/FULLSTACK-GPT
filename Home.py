ec5aea0f3b2c390a8dd331af5db1eee59de83302
CrystalKim1216 <crystalkim1216@gmail.com>
2025-06-17 20:35:49 +0900

    add documentGPT

---
+++ Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+  page_title="GPT Home",
+  page_icon="🤖",
+)
+
+st.markdown(
+  """
+# Hello!
+
+Here are the apps I made:
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteQPT](/SiteQPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
+"""
+)
